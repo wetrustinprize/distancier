ed41fafe7bcb306a0cbfe1a226876e487a99fa8a
Peterson Adami Candido <contact.petersonac@protonmail.com>
2022-03-31 15:18:42 -0300

    copy lat and lng when click on map + select marker on click

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Map from "@components/Map";
 import MapMarker from "@components/MapMarker";
+import MapMarkerDistances from "@components/MapMarkerDistances";
 import Sidebar from "@components/Sidebar";
 import { MarkersContext } from "@contexts/MarkersContext";
 import { Wrapper } from "@googlemaps/react-wrapper";
@@ -10,14 +11,26 @@ import { useContext } from "react";
 import styled from "./styles.module.scss";
 
 const Home: NextPage = () => {
-  const { markers } = useContext(MarkersContext);
+  const { markers, selectMarker } = useContext(MarkersContext);
+
+  // Copy to the clipboard the lat and lng when click
+  const copyLatLng = (e: google.maps.MapMouseEvent) => {
+    if (!e.latLng) return;
+
+    navigator.clipboard.writeText(`${e.latLng.lat()},${e.latLng.lng()}`);
+  };
 
   return (
     <Wrapper apiKey={ENVIRONMENT.googleMapsApiKey}>
       <div className={styled.container}>
-        <Map>
+        <Map onClick={copyLatLng}>
+          <MapMarkerDistances />
           {markers.map((marker, index) => (
-            <MapMarker key={index} mark={marker} />
+            <MapMarker
+              onClick={() => selectMarker(marker)}
+              key={index}
+              mark={marker}
+            />
           ))}
         </Map>
         <Sidebar />
