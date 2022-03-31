8eaec7c5f8ae054697428797393582c14d5f9904
Peterson Adami Candido <contact.petersonac@protonmail.com>
2022-03-31 15:17:51 -0300

    `onClick` marker callback

--- src/components/MapMarker/index.tsx
+++ src/components/MapMarker/index.tsx
@@ -1,20 +1,26 @@
 import { Mark } from "@interfaces/Mark";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 interface IMapMarkerProps {
   mark: Mark;
+  onClick?: () => void;
   map?: google.maps.Map;
 }
 
 const MapMarker: React.FC<IMapMarkerProps> = ({
   mark,
+  onClick,
   map,
 }: IMapMarkerProps) => {
-  const [marker, setMarker] = React.useState<google.maps.Marker>();
+  const [marker, setMarker] = useState<google.maps.Marker>();
 
-  React.useEffect(() => {
+  useEffect(() => {
     if (!marker) {
-      setMarker(new google.maps.Marker());
+      const marker = new google.maps.Marker();
+
+      if (onClick) marker.addListener("click", onClick);
+
+      setMarker(marker);
     }
 
     // remove marker from map on unmount
@@ -25,7 +31,7 @@ const MapMarker: React.FC<IMapMarkerProps> = ({
     };
   }, [marker]);
 
-  React.useEffect(() => {
+  useEffect(() => {
     if (marker) {
       marker.setOptions({
         position: mark.position,
